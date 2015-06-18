46ca7b0bef74cb09c13e2cb7d3d3e5ba04b4f955
Wanderlei Santos <wanderleidias.santos@hotmail.com>
2015-06-18 13:08:19 -0300

    Mudança para netbeans.

---
+++ SicomercioWS/src/main/java/br/com/gdgtresrios/sicomerciows/resource/HelloWorldResource.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.gdgtresrios.sicomerciows.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/helloworld")
+public class HelloWorldResource {
+    
+    @GET
+    @Produces("application/json; charset=UTF-8")
+    public String hello() {
+            return "Hello World";
+    }
+    
+}
