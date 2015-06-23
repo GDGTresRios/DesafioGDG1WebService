0630f0d188953379bed4d78bcda66de7d1b4701c
Wanderlei Santos <wanderleidias.santos@hotmail.com>
2015-06-22 23:46:50 -0300

    Criando método listarTodos()

--- SicomercioWS/src/main/java/br/com/gdgtresrios/sicomerciows/resource/CategoriasEventosResource.java
+++ SicomercioWS/src/main/java/br/com/gdgtresrios/sicomerciows/resource/CategoriasEventosResource.java
@@ -11,35 +11,53 @@ package br.com.gdgtresrios.sicomerciows.resource;
  */
 import br.com.gdgtresrios.sicomerciows.resource.dao.CategoriasEventosDAO;
 import br.com.gdgtresrios.sicomerciows.resource.models.CategoriasEventos;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-
 @Path("/categoriaseventos")
 public class CategoriasEventosResource {
-    
+
     @GET
     @Path("/categoriaevento/{id}")
     @Produces("application/json; charset=UTF-8")
-    public String getCategoriasEventosByID(@PathParam("id") int id) throws IOException {       
-        
+    public String getCategoriasEventosByID(@PathParam("id") int id) throws IOException {
+
         CategoriasEventosDAO eventosDAO = new CategoriasEventosDAO();
         CategoriasEventos categoriasEventos = eventosDAO.getCategoriasEventosByID(id);
-        
+
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
-    
+
         StringWriter writer = new StringWriter();
-    
+
         objectMapper.writeValue(writer, categoriasEventos);
-               
+
         return writer.toString();
     }
-    
+
+    @GET
+    @Path("/listartodos")
+    @Produces("application/json; charset=UTF-8")
+    public String getAll() throws JsonProcessingException, IOException {
+
+        CategoriasEventosDAO eventosDAO = new CategoriasEventosDAO();
+        List<CategoriasEventos> categoriasEventos = eventosDAO.getAll();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+
+       //StringWriter writer = new StringWriter();    
+        //objectMapper.writeValue(writer, categoriasEventos);
+        //return writer.toString();
+        return objectMapper.writeValueAsString(categoriasEventos);
+    }
+
 }
--- SicomercioWS/src/main/java/br/com/gdgtresrios/sicomerciows/resource/dao/CategoriasEventosDAO.java
+++ SicomercioWS/src/main/java/br/com/gdgtresrios/sicomerciows/resource/dao/CategoriasEventosDAO.java
@@ -10,6 +10,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -17,6 +19,7 @@ import java.sql.SQLException;
  */
 public class CategoriasEventosDAO {
 
+    private static final String SQL_SELECT_ALL = "SELECT id, nome FROM gdg_centrosul.categorias_eventos";
     private static final String SQL_SELECT_BY_ID = "SELECT id, nome FROM gdg_centrosul.categorias_eventos WHERE id = ?";
 
     public CategoriasEventos getCategoriasEventosByID(int id) {
@@ -32,8 +35,8 @@ public class CategoriasEventosDAO {
             rs = pstm.executeQuery();
             
             while (rs.next()){
-                categoriasEventos.setId(rs.getInt(1));
-                categoriasEventos.setNome(rs.getString(2));
+                categoriasEventos.setId(rs.getInt("id"));
+                categoriasEventos.setNome(rs.getString("nome"));
             }
             
         } catch (SQLException ex) {
@@ -43,6 +46,35 @@ public class CategoriasEventosDAO {
         
         return categoriasEventos;
         
+    }
+    
+    public List<CategoriasEventos> getAll(){
+        
+        List<CategoriasEventos> categoriasEventos = new ArrayList<>();
+        
+        Connection conn = ConnectionFactory.getConnection();        
+        PreparedStatement pstm = null;
+        ResultSet rs = null;
+        
+        try{
+            pstm = conn.prepareStatement(SQL_SELECT_ALL);
+            rs = pstm.executeQuery();
+            
+            while (rs.next()){
+                
+                CategoriasEventos ct = new CategoriasEventos();
+                ct.setId(rs.getInt("id"));
+                ct.setNome(rs.getString("nome"));
+                
+                categoriasEventos.add(ct);
+            }
+        }catch (SQLException ex){
+            
+        } finally {
+          ConnectionFactory.close(conn, pstm, rs);  
+        }
+        
+        return categoriasEventos;
     }
 
 }
